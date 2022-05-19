1759f158301f9e3210799486843af976016718b0
Aleksandr <a.v.smirnov1992@gmail.com>
2022-05-19 15:08:01 +0300

    Создан тестовый проект для работы с Twig

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//require __DIR__.'/vendor/autoload.php';
+require_once '/path/to/vendor/autoload.php';
+
+
+$loader = new Twig_Loader_String();
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'));
